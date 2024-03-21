49e5ae662267236fdf3516d0bc2b1e79e8130374
Mujtaba Rasheed <mr.muju139@gmail.com>
2024-03-21 22:50:41 +0500

    Quesion 7 of adding in the 45  assignemnt question

---
+++ question 7/q7.js
@@ -0,0 +1,17 @@
+/*Number Eight: Write addition, subtraction, multiplication, and division operations that each
+result in the number 8. Be sure to enclose your operations in print statements to see the
+results. */
+//assign a variable for each variable
+//for addition
+var add = 4 + 4;
+//for subtraction
+var subtract = 14 - 6;
+//for multiplication
+var multiply = 2 * 4;
+//for subtraction
+var divide = 16 / 2;
+//print the each statement by using the variable
+console.log("The sum is ".concat(add));
+console.log("The subtraction is ".concat(subtract));
+console.log("The multiplication is ".concat(multiply));
+console.log("The division is ".concat(divide));
---
+++ question 7/q7.ts
@@ -0,0 +1,27 @@
+/*Number Eight: Write addition, subtraction, multiplication, and division operations that each
+result in the number 8. Be sure to enclose your operations in print statements to see the 
+results. */
+
+//assign a variable for each variable
+
+//for addition
+let add:number=4+4;
+
+//for subtraction
+let subtract:number=14-6;
+
+//for multiplication
+let multiply:number=2*4;
+
+//for subtraction
+let divide:number=16/2;
+
+//print the each statement by using the variable
+
+console.log(`The sum is ${add}`)
+
+console.log(`The subtraction is ${subtract}`)
+
+console.log(`The multiplication is ${multiply}`)
+
+console.log(`The division is ${divide}`)
