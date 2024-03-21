a880dda01d0fa9676daff2850b5035a3eae4d1c6
Mujtaba Rasheed <mr.muju139@gmail.com>
2024-03-21 22:05:38 +0500

    This commit is for adding the Question 5 of 45 days of assignement

---
+++ question5/q5.js
@@ -0,0 +1,8 @@
+/*Famous Quote: Find a quote from a famous person you admire.
+Print the quote and the name of its author */
+//store the name of the author in variabel
+var author_name = "Imam Ali ";
+//store the quite in variable
+var quote = "The TONGUE is like a lion, if you let it loose it will be wound someone. ";
+//print the statement
+console.log("".concat(author_name, " once said: ").concat(quote));
---
+++ question5/q5.ts
@@ -0,0 +1,14 @@
+/*Famous Quote: Find a quote from a famous person you admire.
+Print the quote and the name of its author */
+
+//store the name of the author in variabel
+
+let author_name:string="Imam Ali ";
+
+//store the quite in variable
+
+let quote:string="The TONGUE is like a lion, if you let it loose it will be wound someone. ";
+
+//print the statement
+
+console.log(`${author_name} once said: ${quote}`)
