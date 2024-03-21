4da22962e07e8f5be0b53b04689d2ac552585fa3
Mujtaba Rasheed <mr.muju139@gmail.com>
2024-03-21 11:44:01 +0500

    This is the question 3 of the 45 assignment question

---
+++ question 3/q3.js
@@ -0,0 +1,8 @@
+/*Name Cases: Store a person’s name in a variable, and then print that person’s
+name in lowercase, uppercase, and titlecase. */
+//store a name in variabel
+var PersonName = "Abdul rasheed malik";
+//in upper case
+console.log("The letter in upper case is: " + PersonName.toUpperCase());
+//in lower case
+console.log("The letter in lower case is: " + PersonName.toLowerCase());
---
+++ question 3/q3.ts
@@ -0,0 +1,12 @@
+/*Name Cases: Store a person’s name in a variable, and then print that person’s
+name in lowercase, uppercase, and titlecase. */
+
+//store a name in variabel
+
+let PersonName:string="Abdul rasheed malik";
+
+//in upper case
+console.log("The letter in upper case is: "+PersonName.toUpperCase())
+
+//in lower case
+console.log("The letter in lower case is: "+PersonName.toLowerCase())
