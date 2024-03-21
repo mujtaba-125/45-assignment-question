0e9e307fa0baf79c6d6367434509c9f6869b4183
Mujtaba Rasheed <mr.muju139@gmail.com>
2024-03-21 11:13:52 +0500

    This commit is for the adding question 1 and question 2 of assignment

---
+++ question 2/q2.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Personal Message: Store a person’s name in a variable, and print a message to that person.
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python
+today?”*/
+//create a variable that store the message
+let my_name = "Mujtaba Rasheed Malik";
+//print message
+console.log(`Hello ${my_name}, would you like to learn some Python today?`);
