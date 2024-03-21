5672aa9be5268b6b655706df93751f0456e7f23e
Mujtaba Rasheed <mr.muju139@gmail.com>
2024-03-21 22:36:38 +0500

    adding the question 6 in 45 assignment question

---
+++ question 6/q6.js
@@ -0,0 +1,12 @@
+/*Stripping Names: Store a person’s name, and include some whitespace characters at the
+beginning and end of the name. Make sure you use each character combination, "\t" and "\n",
+at least once. Print the name once, so the whitespace around the name is displayed. Then print
+the name after striping the white spaces.*/
+//use each character combination, "\t" in new variable
+var student_name1 = "\tSaad Tahir";
+//use each character combination, "\n" in new variable
+var student_name2 = "\nImran Nazeer";
+//print the first student with \t combination
+console.log("character combination, tab spaace " + student_name1 + ".");
+//print the first student with \t combination
+console.log("character combination, new line spaace " + student_name2 + ".");
---
+++ question 6/q6.ts
@@ -0,0 +1,17 @@
+/*Stripping Names: Store a person’s name, and include some whitespace characters at the 
+beginning and end of the name. Make sure you use each character combination, "\t" and "\n",
+at least once. Print the name once, so the whitespace around the name is displayed. Then print
+the name after striping the white spaces.*/
+
+//use each character combination, "\t" in new variable
+let student_name1:string="\tSaad Tahir";
+
+//use each character combination, "\n" in new variable
+let student_name2:string="\nImran Nazeer";
+
+//print the first student with \t combination
+console.log("character combination, tab space "+student_name1+".")
+
+//print the first student with \t combination
+
+console.log("character combination, new line spaace "+student_name2+".")
