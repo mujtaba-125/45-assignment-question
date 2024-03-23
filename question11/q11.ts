0827d9d53f2b7ea8bd36a59fba5e3123ab5f5135
Mujtaba Rasheed <mr.muju139@gmail.com>
2024-03-23 06:47:27 +0500

    solving the question 11 and 12

---
+++ question11/q11.ts
@@ -0,0 +1,19 @@
+/*Names: Store the names of a few of your friends in a array called names. Print each person’s 
+name by accessing each element in the list, one at a time.*/
+
+//create a variable that store the list of friends
+let  friends_name=["Razeen","Taha","Talha","danish"]
+
+//accessing each element present in a list
+
+//accessing 1st element present at 0th index
+console.log(friends_name[0])
+
+//accessing 2nd element present at 1st index
+console.log(friends_name[1])
+
+//accessing 3rd element present at 2nd index
+console.log(friends_name[2])
+
+//accessing 4th element present at 3rd index
+console.log(friends_name[3])
---
+++ question12/q12.js
@@ -0,0 +1,16 @@
+/*Greetings: Start with the array you used in Exercise 11, but instead of just printing
+each person’s name, print a message to them. The text of each message should be the same,
+ but each message should be personalized with the person’s name.*/
+//create a variable that store the list of friends
+var friends_name = ["Razeen", "Taha", "Talha", "danish"];
+//create greeting variable
+var greetings = "Assalamualikum, how do you do? ";
+//accessing each element present in a list
+//accessing 1st element present at 0th index
+console.log(greetings + friends_name[0]);
+//accessing 2nd element present at 1st index
+console.log(greetings + friends_name[1]);
+//accessing 3rd element present at 2nd index
+console.log(greetings + friends_name[2]);
+//accessing 4th element present at 3rd index
+console.log(greetings + friends_name[3]);
