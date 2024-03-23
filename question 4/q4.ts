6de29f5c3ce7c439da72e7efc42df96c1cd0918a
Mujtaba Rasheed <mr.muju139@gmail.com>
2024-03-23 05:58:19 +0500

    direct write the statement without store in variable

--- question 4/q4.ts
+++ question 4/q4.ts
@@ -1,6 +1,6 @@
 /*Famous Quote: Find a quote from a famous person you admire.
 Print the quote and the name of its author */
 
-//print the statement
+//print the quote
 
 console.log(`Imam Ali once said: knowledge needs action`)
